db65d667cdb7c96a2e6ed3d6dceed0d654ebf688
Ognian Genev <39865181+ogenev@users.noreply.github.com>
2019-03-27 14:17:23 +0200

    Add files via upload

---
+++ test_agg.py
@@ -0,0 +1,47 @@
+import merger
+import pytest
+import os
+import pandas
+import numpy
+
+class Test_Aggregators(object):
+
+    @pytest.fixture(scope="class")
+    def load_data(self):
+
+        #Load Jan's data
+        this_merger = merger.Merger()
+        this_merger.folder.input_folder = '../test_data/'
+        this_merger.params.contract = 'F_BRN_'
+        files_dict = this_merger.make_file_list()
+        keys = files_dict.keys()
+        tick_df_list = []
+        for key in keys:
+            for file_name in files_dict[key].FILE_NAME:
+                file_in     =  os.path.join(*[this_merger.folder.input_folder, file_name])
+                tick_df = pandas.read_csv(file_in,
+                                              compression  = 'gzip')
+
+                #Fill expiration date
+                tick_df['EXPIRATION_DATE'] = 20190616
+
+                #Drop non-trade columns
+                tick_df = tick_df.dropna(subset = ['TRADE_SIZE'])
+                
+                #Pad prices
+                list_columns        = ['BID_SIZE', 'ASK_SIZE', 'BID_PRICE', 'ASK_PRICE']
+                tick_df['datetime'] = pandas.to_datetime(tick_df.ACTIVITY_DATETIME,
+                                                 utc  = False)
+
+                tick_df = tick_df.pipe(this_merger.select_padder,
+                            list_columns = list_columns)
+
+
+                tick_df_list.append(tick_df)
+
+        return tick_df_list
+        
+    def test_aggreg_data(self, load_data):
+        #this_merger.aggregation_time_factor = '1min'
+        print(load_data)
+        assert 1 == 1
